5c0032206fe99ef9e37ee192a6943ccf75c79746
manetti19 <manetti.biazon@ime.eb.br>
2025-03-04 12:34:55 -0300

    asd

--- teste.cpp
+++ teste.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    std::cout << "Hello worldddd" << std::endl;
+    return 0;
 }
+
+
+
+// sempre "Ctrl + S" antes de dar Run
